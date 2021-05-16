fea1e67a8e26cbccab66c9266add0fb8c918023d
jorgesilva97 <36448@ufp.edu.pt>
2021-05-16 21:48:40 +0100

    array

--- src/edu/ufp/inf/sd/project/server/JobGroupImpl.java
+++ src/edu/ufp/inf/sd/project/server/JobGroupImpl.java
@@ -1,14 +1,19 @@
 package edu.ufp.inf.sd.project.server;
 
+import edu.ufp.inf.sd.project.client.Worker;
+import java.util.ArrayList;
+
 public class JobGroupImpl implements JobGroupRI
 {
 
     private int jobId;
     private String user;
     //array de workers disponiveis
+    ArrayList<Worker> workersDisponiveis = new ArrayList<>();
 
 
     //adiciona worker
+
 
 
 }
